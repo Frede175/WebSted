661fb4fe2438d68d0aaca16a74776c573c661cb5
Frede175 <fsr1998@hotmail.com>
2015-02-04 20:46:33 +0100

    Animate fix :dancer:

--- js/main.js
+++ js/main.js
@@ -2,12 +2,17 @@ $(document).ready(function() {
 
 	$(".navbut").mouseenter(function() {
 		$(this).animate ({
-				"background-color": "rgba(15,15,15,0.7)",
-		}, 500, function());
+			backgroundColor: "rgba(15,15,15,0.7)",
+		}, 500, function() {
+
+		});
+	});
 
 	$(".navbut").mouseleave(function() {
-		$(this).animate ([
-				"background-color": "rgba(256,256,256,0)",
-		], 500, function());
+		$(this).animate ({
+				backgroundColor: "rgba(256,256,256,0)",
+		}, 500, function() {
+
+		});
 	});
 });
