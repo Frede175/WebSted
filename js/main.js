758df1764e0df20963ab1173343631a752ac55a4
TheCodingNinja <madsholmjensen.mail@gmail.com>
2015-02-04 21:19:37 +0100

    minor changes to header animation color :+1:
    Mads

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
 
-	$(".navbut").mouseenter(function() {
-		$(this).animate ({
-			backgroundColor: "rgba(15,15,15,0.7)",
-		}, 500, function() {
 
-		});
+	$(".navbut").mouseenter(function() {
+			$(this).animate ({
+				backgroundColor: "rgba(15,15,15,0.7)",
+			}, 250, function() {
+
+			});
 	});
 
-	$(".navbut").mouseleave(function() {
-		$(this).animate ({
-				backgroundColor: "rgba(256,256,256,0)",
-		}, 500, function() {
+	$(".navbut").mouseleave(function() {	
+			$(this).animate ({
+					backgroundColor: "rgba(15,15,15,0)",
+			}, 250, function() {
 
-		});
+			});
 	});
 });
